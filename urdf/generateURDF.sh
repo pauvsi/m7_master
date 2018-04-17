835ecd950b8be0ef4d05758c926a5e56821cc188
Trexter <sheridak@purdue.edu>
2018-04-17 01:03:15 -0400

    compiled urdf and added launch file

---
+++ urdf/generateURDF.sh
@@ -0,0 +1,1 @@
+rosrun xacro xacro -o  noir_base.urdf noir_description/urdf/noir_base.xacro enable_mavlink_interface:=true enable_ground_truth:=true enable_wind:=false enable_logging:=true rotors_description_dir:=../
