11e184f8d01a43869485ad620a9d1d35a631a76b
Trexter <sheridak@purdue.edu>
2018-05-09 19:36:00 -0400

    put quad in iarc world

--- robot_description/generateURDF.sh
+++ robot_description/generateURDF.sh
@@ -1,4 +1,5 @@
 rm -f noir.sdf
+rm -f noir_base.urdf
 rosrun xacro xacro -o  noir_base.urdf noir_description/urdf/noir_base.xacro enable_mavlink_interface:=true enable_ground_truth:=true enable_wind:=false enable_logging:=true rotors_description_dir:=../
 gz sdf -p  noir_base.urdf >> noir.sdf
-rm -f noir_base.urdf
+
